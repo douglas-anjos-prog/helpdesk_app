776a9db12b191558d9ffd6e62c75ddbdeb858552
douglas-anjos-prog <doughz071@gmail.com>
2025-04-23 16:31:08 -0300

    atualizando o codigo

--- index.php
+++ index.php
@@ -39,10 +39,23 @@
                 <div class="form-group">
                   <input name="senha" type="password" class="form-control" placeholder="Senha" required>
                 </div>
+                <?php 
+                  if(isset($_GET['login']) && $_GET['login'] == 'erro'){ ?>
+                <div class="text-danger">
+                  Usuário ou senha inválido(s).
+                </div>
+                <?php } ?>
+                <?php 
+                  if(isset($_GET['login']) && $_GET['login'] == 'erro2'){ ?>
+                <div class="text-danger">
+                  Faça login para acessar a página.
+                </div>
+                <?php } ?>
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
           </div>
+
         </div>
     </div>
   </body>
--- valida_login.php
+++ valida_login.php
@@ -1,9 +1,25 @@
 <?php 
-    print_r($_POST);
-    echo '<br/>';
-    echo htmlspecialchars($_POST['email'] ?? '');
-    echo '<br/>';
-    echo htmlspecialchars($_POST['senha'] ?? '');
+    session_start();
+
+    //Variavel que verifica se a autenticacao foi realizada
+    $usuario_atenticado = false;
+    //Relacao de usuarios dos sistemas
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' =>'123456'),
+        array('email' => 'user@teste.com.br', 'senha' =>'abcdef'),
+    );
+foreach($usuarios_app as $user){
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+        $usuario_atenticado = true;
+    }
+}
+if($usuario_atenticado){
+    echo 'Usuário autenticado';
+    $_SESSION['autenticado'] = 'SIM';
+} else {
+    $_SESSION['autenticado'] = 'NAO';
+    header('Location: index.php?login=erro');
     
-    echo "Formulário enviado";
+}
+
 ?>
